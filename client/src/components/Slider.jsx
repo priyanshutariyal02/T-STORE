9e725b3b0d5f30fcfb855f86861dc728b9f2e06a
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-05-03 11:51:52 +0530

    complete slider

---
+++ client/src/components/Slider.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
+import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
+import "./Slider.scss";
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  function prevSlide() {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  }
+
+  function nextSlide() {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+  }
+  const data = [
+    "https://images.unsplash.com/photo-1519638831568-d9897f54ed69?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+  return (
+    <div className="slider">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <WestOutlinedIcon />
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <EastOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
